c3969ef1534db15d7cfd44f4f55d55b4a37dd46c
19424051 <rynerlelouch@gmail.com>
2021-01-07 22:27:34 +0700

    init FE

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Login from './components/login';
+
+function App() {
+  return (
+    <div className="container">
+      <center><h2>Login</h2></center>
+      <Login />
+    </div>
+  );
+}
+
+export default App;
