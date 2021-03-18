085b84910971cd8a755780b405b990cf006b337a
19424051 <rynerlelouch@gmail.com>
2021-03-19 01:52:40 +0700

    reset home

--- src/views/onlineAcademy.js
+++ src/views/onlineAcademy.js
@@ -8,6 +8,7 @@ import ApppContext from '../onlineAcademyAppContext';
 import { axiosInstance } from '../utils';
 import Resultcategories from './resultCategories';
 import Profile from './profile';
+import HotCourses from './hotcourses';
 
 export default function OnlineAcademy() {
     const initialAppState = {
@@ -68,22 +69,56 @@ export default function OnlineAcademy() {
                 });
             }
         }
+        // async function loadDataUser() {
+        //     const res = await axiosInstance.get('/users/' + localStorage.account_userID, { headers: { 'x-access-token': localStorage.account_accessToken } });
+        //     if (res.status === 200) {
+        //         res.data.watchlistJS = res.data.watchlist ? JSON.parse(res.data.watchlist).course : [];
+        //         dispatch({
+        //             type: 'setAccount',
+        //             payload: {
+        //                 account: res.data,
+        //                 query: '',
+        //             }
+        //         });
+        //     }
+        // }
+        // async function loadDataPayment() {
+        //     const res = await axiosInstance.get('/transaction/user/' + localStorage.account_userID, { headers: { 'x-access-token': localStorage.account_accessToken } });
+        //     if (res.status === 200) {
+        //         dispatch({
+        //             type: 'setPayment',
+        //             payload: {
+        //                 payment: res.data,
+        //                 query: '',
+        //             }
+        //         });
+        //     }
+        // }
         initCoursesList();
         getCategory();
         getTeacher();
         getFeedback();
+        // if(localStorage.account_accessToken){
+        //     loadDataPayment();
+        // }
     }, []);
    
-
     return (
         <div>
             <ApppContext.Provider value={{ store, dispatch }}>
                 <Header />
                 {/* <HeaderPopup />
                 <HeaderPrimary /> */}
+                {/* <HotCourses /> */}
+                {/* {(() => {
+                    if (store.mode === 'default') {
+                            return <HotCourses />
+                    }
+                })()} */}
                 {(() => {
                     if (store.mode === 'default') {
-                        return <HomeContent />
+                            return [<HotCourses />,
+                                <HomeContent />];
                     } else if (store.mode === 'search') {
                         return <Resultcategories />
                     } else if (store.mode === 'profile') {
